67c257c9d55b8658f3ef6ac088c95fa547f89c01
NicklasRitola <61377039+NicklasRitola@users.noreply.github.com>
2021-04-28 18:06:23 +0200

    Test för sockets, serversidan.

---
+++ ServerSide/ServerSide/Prog.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ServerSide
+{
+    class Prog
+    {
+        static void Main(string[] args)
+        {
+            executeServer();
+        }
+        
+        public static void executeServer()
+        {
+            // Establish local endpoint
+
+            IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddr, 11111);
+
+            // Creation TCP/IP socket
+
+            Socket listener = new Socket(ipAddr.AddressFamily,
+                SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                listener.Bind(localEndPoint);
+
+                listener.Listen(10);
+
+                while (true)
+                {
+                    Console.WriteLine("Waiting connection ...");
+
+                    Socket clientSocket = listener.Accept();
+
+                    byte[] bytes = new byte[1024];
+                    string data = null;
+
+                    while (true)
+                    {
+                        int numByte = clientSocket.Receive(bytes);
+
+                        data += Encoding.ASCII.GetString(bytes,
+                            0, numByte);
+
+                        if (data.IndexOf("<EOF>") > -1)
+                            break;
+                    }
+
+                    Console.WriteLine("Text received -> {0} ", data);
+                    byte[] message = Encoding.ASCII.GetBytes("Test Server");
+
+                    clientSocket.Send(message);
+
+                    clientSocket.Shutdown(SocketShutdown.Both);
+                    clientSocket.Close();
+                }
+            }
+
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
