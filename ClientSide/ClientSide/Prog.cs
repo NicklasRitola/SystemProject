1cfa2def2ee85a5b5e355f7b1c396aefb6b5929a
NicklasRitola <61377039+NicklasRitola@users.noreply.github.com>
2021-04-28 18:20:08 +0200

    Test för sockets, Clientsidan.

---
+++ ClientSide/ClientSide/Prog.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ClientSide
+{
+    class Prog
+    {
+        static void Main(string[] args)
+        {
+            executeClient();
+        }
+
+        static void executeClient()
+        {
+            try
+            {
+
+                IPHostEntry ipHost = Dns.GetHostEntry(Dns.GetHostName());
+                IPAddress ipAddr = ipHost.AddressList[0];
+                IPEndPoint localEndPoint = new IPEndPoint(ipAddr, 11111);
+
+                Socket sender = new Socket(ipAddr.AddressFamily,
+                    SocketType.Stream, ProtocolType.Tcp);
+
+                try
+                {
+                    sender.Connect(localEndPoint);
+
+                    Console.WriteLine("Socket connected to -> {0}",
+                        sender.RemoteEndPoint.ToString());
+
+                    byte[] messageSent = Encoding.ASCII.GetBytes("Test Client<EOF>");
+                    int byteSent = sender.Send(messageSent);
+
+                    byte[] messageReceived = new byte[1024];
+
+                    int byteRecv = sender.Receive(messageReceived);
+                    Console.WriteLine("Message from server -> {0}",
+                        Encoding.ASCII.GetString(messageReceived, 0, byteRecv));
+
+                    sender.Shutdown(SocketShutdown.Both);
+                    sender.Close();
+                }
+
+                catch (ArgumentNullException ane)
+                {
+
+                    Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+                }
+
+                catch (SocketException se)
+                {
+
+                    Console.WriteLine("SocketException : {0}", se.ToString());
+                }
+
+                catch (Exception e)
+                {
+                    Console.WriteLine("Unexpected exception : {0}", e.ToString());
+                }
+            }
+
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.ToString());
+            }
+        }
+    }
+}
