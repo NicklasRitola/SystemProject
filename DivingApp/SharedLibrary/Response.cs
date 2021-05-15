da7ad5b6d55f245a90828d73ccebf203f3f98cc9
AdrianSteneryd <addepadde97@yahoo.se>
2021-05-15 17:46:41 +0200

    Updated response
    Added classes in response

--- DivingApp/SharedLibrary/Response.cs
+++ DivingApp/SharedLibrary/Response.cs
@@ -23,5 +23,36 @@ namespace Shared
     public class ScheduleResponse : Response
     {
 
+        public ScheduleResponse()
+        {
+            messageType = "ScheduleResponse";
+        }
+    }
+
+    public class DiverInfoResponse : Response
+    {
+        public string SSN;
+        public string FirstName;
+        public string Surname;
+        public string Gender;
+        public int Age;
+        public int Team;
+        public int Team_ID;
+        public int Diver_ID;
+
+        public DiverInfoResponse()
+        {
+            messageType = "DiverInfoResponse";
+        }
+    }
+
+    public class LoginResponse : Request
+    {
+        public bool Success;
+
+        public LoginResponse()
+        {
+            messageType = "LoginResponse";
+        }
     }
 }
